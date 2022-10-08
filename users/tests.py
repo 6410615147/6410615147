5cb9b72259cd6ab4cf7bf281f2a71043a0f93d8f
6310682726 <69451907+6310682726@users.noreply.github.com>
2022-10-08 17:40:36 +0700

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from quota.models import Subject, Quota, Registant
 
-# Create your tests here.
+class QuotaTestCase(TestCase):
+
+    def setUp(self):
+
+        course1 = Subject.objects.create(code="AAA", name="course1")
+        quota = Quota.objects.create(
+            subject=course1, semester=0, year=2000, seat=2, status=True)
+
+        registant = Registant.objects.create(user="harry")
+        quota.registants.add(registant)
+
+    def test_seat_available(self):
+
+        quota = Quota.objects.first()
+        self.assertTrue(quota.is_seat_available())
+
+    def test_seat_not_available(self):
+
+        registant1 = Registant.objects.create(
+            user="dragon")
+
+        registant2 = Registant.objects.create(
+            user="tiger")
+
+        quota = Quota.objects.first()
+        quota.registants.add(registant1)
+        quota.registants.add(registant2)
+
+        self.assertFalse(quota.is_seat_available())
